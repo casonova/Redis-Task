30f42adb7b69d9057de62774fd172f11d1431d04
Rehan Habib <rehan.habib777@gmail.com>
2023-12-27 11:17:14 +0500

    Added IP Address

--- mainapp/middleware.py
+++ mainapp/middleware.py
@@ -1,30 +1,33 @@
-from django.core.cache import cache
 from django.http import HttpResponse
-from rest_framework.renderers import JSONRenderer
+from django.core.cache import cache
 from rest_framework.response import Response
-
+from rest_framework.renderers import JSONRenderer
 
 class UserMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-
-    def __call__(self, request):
+    
+    def __call__(self, request):    
         if request.user.is_authenticated:
+            
+            ip_address = request.META.get('HTTP_X_FORWARDED_FOR', None)
+            if ip_address:
+                ip_address = ip_address.split(',')[0]
+            else:
+                ip_address = request.META.get('REMOTE_ADDR', '')
             user = request.user
-            print(user)
             user_groups = user.groups.all()
-
-            request_key = f"user_{user.id}_request_count"
+            request_key = f"user {user.username} request_count of ip address {ip_address}"
             print(request_key)
             request_count = cache.get(request_key)
             print(request_count)
-
+            
             if request_count is None:
-                cache.set(request_key, 0, 60)
-                request_count = 0
-
+                cache.set(request_key,0,60)
+                request_count = 0 
+            
             request_limit = 0
-
+            
             for group in user_groups:
                 if group.name == "GOLD":
                     request_limit = 10
@@ -35,16 +38,14 @@ class UserMiddleware:
                 elif group.name == "SILVER":
                     request_limit = 2
                     break
-
+            
             if request_count < request_limit:
                 cache.set(request_key, request_count + 1, 60)
             else:
                 response_data = {"msg": "Too many requests, you are blocked"}
                 renderer = JSONRenderer()
                 rendered_response = renderer.render(response_data)
-                return HttpResponse(
-                    rendered_response, content_type="application/json", status=429
-                )
-
+                return HttpResponse(rendered_response, content_type='application/json', status=429)
+        
         response = self.get_response(request)
         return response
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,16 +1,13 @@
-from django.contrib.auth.models import User
-from rest_framework import status
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-
+from django.contrib.auth.models import User
 from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class UserView(ModelViewSet):
-    authentication_classes = [BasicAuthentication]
+    
     permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
