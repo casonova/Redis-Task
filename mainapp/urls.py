ee2904ad879a277a4bb548e2205b32d140b8d1e1
Rehan Habib <rehan.habib777@gmail.com>
2023-12-27 17:52:07 +0500

    Added IP Address

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,13 +1,17 @@
-from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import UserView
 
-router = routers.DefaultRouter()
-router.register(r"users", UserView, basename="users")
-
 urlpatterns = [
-    path("", include(router.urls)),
+    path(
+        "users/", UserView.as_view({"get": "list", "post": "create"}), name="user-list"
+    ),
+    path(
+        "users/<int:pk>/",
+        UserView.as_view({"get": "retrieve", "put": "update", "delete": "destroy"}),
+        name="user-detail",
+    ),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("mainapp.urls")),
+    path(
+        "login/",
+        views.LoginView.as_view(template_name="rest_framework/login.html"),
+        name="login",
+    ),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("auth/", include("rest_framework.urls")),
 ]
